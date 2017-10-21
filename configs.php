a045a703b51dcd1f7a9e5242c1ad6cb7aa00faef
slik <stanislavdru@gmail.com>
2017-10-21 19:43:40 +0600

    Изменил конфиги подключил к базе хостинга

--- configs.php
+++ configs.php
@@ -1,14 +1,14 @@
 <?php
 
 $db = new DB\SQL(
-  'mysql:host=localhost;port=3306;dbname=shop',
-  'shop',
-  'root'
+  'mysql:host=localhost;port=3306;dbname=id3330757_shop',
+  'id3330757_slik_9',
+  'gremlin12'
 );
 
-define('SITE_EMAIL', 'support@shop.us');
+define('SITE_EMAIL', 'slik_9@mail.ru');
 define('SITE_NAME', 'shop');
-define('BASE_URL', 'http://127.0.0.1/shop');
+define('BASE_URL', 'https://slik-9.000webhostapp.com');
 define('SITE_URL', ((isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == 'on') ? 'https://' : 'http://') . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']);
 
 $f3->set('DEBUG', 3);
