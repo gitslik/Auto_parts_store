a4cda004a0da73480a5cb2f7ecd06886015555d5
slik <stanislavdru@gmail.com>
2017-12-03 22:31:43 +0600

    Перенёс на новый сервер

--- configs.php
+++ configs.php
@@ -1,23 +1,14 @@
 <?php
 
 $db = new DB\SQL(
-  'mysql:host=localhost;port=3306;dbname=id3330757_shop',
-  'id3330757_slik_9',
+  'mysql:host=mysql.hostinger.ru;port=3306;dbname=u258501922_taru',
+  'u258501922_taru',
   'gremlin12'
 );
 
 define('SITE_EMAIL', 'slik_9@mail.ru');
 define('SITE_NAME', 'shop');
-define('BASE_URL', 'https://slik-9.000webhostapp.com');
+define('BASE_URL', 'http://slik-9.xyz');
 define('SITE_URL', ((isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == 'on') ? 'https://' : 'http://') . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']);
 
 $f3->set('DEBUG', 3);
-/*$f3->set('ONERROR',
-  function($f3) {
-    // recursively clear existing output buffers:
-    while (ob_get_level())
-      ob_end_clean();
-    // your fresh page here:
-    echo $f3->get('ERROR.text');
-  }
-);*/
