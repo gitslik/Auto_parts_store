604c95725fbecba60b70d0917c8d79b80a6625e6
slik <stanislavdru@gmail.com>
2018-01-07 21:49:45 +0600

    Не доделал youtube

--- view/admin/youtube/index.php
+++ view/admin/youtube/index.php
@@ -14,8 +14,36 @@
 Навигация для управиления Youtube меню на главной странице.
 
 <div>
-  <form id="youtube_form" action="" method="post">
+  <form id="youtube_form" action="/admin/addYoutube" method="post">
     <input type="text" name="youtube_name" id="youtube_element">
     <input type="submit"  value="Save">
   </form>
 </div>
+
+<script>
+
+  $("#youtube_form").submit(function( event ) {
+    event.preventDefault();
+
+    var url_video = $('#youtube_element').val();
+
+    console.log(url_video);
+
+    if(url_video){
+      var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*!/;
+      var match = url_video.match(regExp);
+
+      console.log(match);
+
+/*      var productInputs = "<div class='video-block-ssw'><iframe src='https://www.youtube.com/embed/"+getId(url_video)+"' width='90%' height='360' frameborder='0' allowfullscreen></iframe></div>";
+      ssw(ssw(".grid.product-single>div")[0]).append(productInputs);
+      var productInputs_mobile = "<div class='video-block-ssw-mobile'><iframe src='https://www.youtube.com/embed/"+getId(url_video)+"' width='100%' height='360' frameborder='0' allowfullscreen></iframe></div>";
+      ssw(".description").append(productInputs_mobile);*/
+
+    }
+
+
+  });
+
+
+</script>
