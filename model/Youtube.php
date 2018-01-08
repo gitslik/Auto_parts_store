aa6eb508310629dcf02b67c1776249e28549fdd9
slik <stanislavdru@gmail.com>
2018-01-08 20:57:07 +0600

    Не доделал youtube

---
+++ model/Youtube.php
@@ -0,0 +1,34 @@
+<?php
+
+class Youtube extends DB\SQL\Mapper
+{
+
+  public function __construct(DB\SQL $db)
+  {
+    parent::__construct($db, 'youtube');
+  }
+
+
+
+  static function addYoutubeItem($id)
+  {
+    global $db;
+    if (isset($id) && $id != "") {
+      $youtube_url = $db->exec("SELECT * FROM youtube WHERE id = 1");
+
+      if (count($youtube_url)==0) {
+
+       $result = $db->exec("INSERT INTO youtube SET id_youtube = {$id}");
+      } elseif (count($youtube_url)>1) {
+
+       $result = $db->exec("UPDATE youtube SET id_youtube = {$id} WHERE id = 1");
+      } else {
+       $result = false;
+      }
+    }
+    return $result;
+  }
+
+}
+
+?>
