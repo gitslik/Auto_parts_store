d278e5a1803bf32cdf3faac74c9c62a4318f0cdc
slik <stanislavdru@gmail.com>
2018-01-11 15:13:00 +0600

    Добавил на главную в одминку статистику письма и заказы недоделал

--- view/admin/index.php
+++ view/admin/index.php
@@ -1,12 +1,32 @@
 <div id="page-wrapper">
   <div class="content_page col-lg-12">
+
+    <style>
+      div#page-wrapper {
+        min-height: 460px;
+      }
+    </style>
+
     <div class="row">
       <div class="col-lg-12">
         <h1 class="page-header">Информация</h1>
       </div>
     </div>
 
-    Какой то контент.
+    <div class="mini_menu">
+      <div class="menu_td" onclick="self.shop.footer('info')">
+        <img src="../image/pisma.jpeg" class="menu_img">
+        <p class="menu_text">ПИСЬМА</p>
+      </div>
+      <div class="menu_td" onclick="self.shop.footer('subscription')">
+        <img src="../image/zakazy.jpg" onclick="" class="menu_img">
+        <p class="menu_text">ЗАКАЗЫ</p>
+      </div>
+      <div class="menu_td" onclick="self.shop.footer('collbeack')">
+        <img src="../image/prosmotry.jpg" class="menu_img">
+        <p class="menu_text">ПРОСМОТРЫ</p>
+      </div>
+    </div>
 
   </div>
 </div>
